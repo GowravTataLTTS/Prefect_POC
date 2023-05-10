0c9ab38e887041416d098b6309d4f22f48eea68b
GowravTataLTTS <gowrav.bhargav@ltts.com>
2023-05-10 20:14:36 +0530

    sada

--- task_all.py
+++ task_all.py
@@ -5,7 +5,7 @@ Created on Mon Apr 17 19:45:04 2023
 """
 
 import time
-from models import Customers, CustomersInsert, CustomersUpdate, CustomersDelete, CustomersDone
+from models import Customers, CustomersInsert, CustomersUpdate, CustomersDelete
 from prefect import task, flow
 from subprocess import PIPE, Popen
 import schedule
