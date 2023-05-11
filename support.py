f02efe0a9e63ffb258a943da229cda0e20b7ad03
GowravTataLTTS <gowrav.bhargav@ltts.com>
2023-05-11 09:07:04 +0530

    support file

---
+++ support.py
@@ -0,0 +1,39 @@
+from models import Customers, CustomersInsert, CustomersUpdate, CustomersDelete
+from datetime import datetime
+from sqlalchemy import delete
+from sqlalchemy import MetaData
+from task_all import transaction
+
+metadata = MetaData()
+
+
+def retrieve_data():
+    print(datetime.now().strftime("%H:%M:%S"), 'Received Data')
+    with transaction() as session:
+        return (
+            session.query(Customers.name, Customers.country, Customers.phone, Customers.email)
+                .all()
+        )
+
+
+def transformation(data):
+    complete_data = []
+    for i in data:
+        record = {'name': i.name, 'country': i.country, 'phone': i.phone,
+                  'email': i.email}
+        complete_data.append(record)
+    with transaction() as session:
+        session.execut(delete(CustomersInsert).all())
+        session.bulk_insert_mappings(CustomersUpdate, complete_data)
+        session.bulk_insert_mappings(CustomersDelete, complete_data)
+        print("Finished")
+
+
+def checker():
+    all_data = retrieve_data()
+    transformation(data=all_data)
+    return
+
+
+if __name__ == '__main__':
+    checker()
