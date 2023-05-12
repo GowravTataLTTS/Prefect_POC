ecbc66e1d4cdb7e215f18a80a89731f9fa504c58
GowravTataLTTS <gowrav.bhargav@ltts.com>
2023-05-12 17:24:52 +0530

    union models

--- million_models.py
+++ million_models.py
@@ -13,14 +13,12 @@ class Customers(Base):
     country = Column(Text)
     phone = Column(Text)
     email = Column(Text)
-    id = Column(Integer, primary_key=True)
 
-    def __init__(self, name, country, phone, email, id):
+    def __init__(self, name, country, phone, email):
         self.name = name
         self.country = country
         self.phone = phone
         self.email = email
-        self.id = id
 
     def __repr__(self):
         return f"{self.name},{self.country},{self.phone},{self.email}"
@@ -32,14 +30,12 @@ class CustomersInsert(Base):
     country = Column(Text)
     phone = Column(Text)
     email = Column(Text)
-    id = Column(Integer, primary_key=True)
 
-    def __init__(self, name, country, phone, email, id):
+    def __init__(self, name, country, phone, email):
         self.name = name
         self.country = country
         self.phone = phone
         self.email = email
-        self.id = id
 
     def __repr__(self):
         return f"{self.name},{self.country},{self.phone},{self.email}"
