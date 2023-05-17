20dd8832668c911ac163bfaa1e16a4fa3afe36cc
GowravTataLTTS <gowrav.bhargav@ltts.com>
2023-05-17 13:01:06 +0530

    new logsss

--- script_for_million_records.py
+++ script_for_million_records.py
@@ -9,6 +9,7 @@ def trigger():
     first_data = retrieve_data()
     print(datetime.now().strftime("%H:%M:%S"), 'Ended Fetching Delta Data')
     print(datetime.now().strftime("%H:%M:%S"), 'Number of records are', len(first_data))
+    print(datetime.now().strftime("%H:%M:%S"), 'Fetched records are', first_data)
     #updated_data = transformation(first_data)
     return
 
